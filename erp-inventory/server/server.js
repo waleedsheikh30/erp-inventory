e27c29c72f456a0667275adbe9b5b3bae80cce33
Waleed Sheikh <125444427+waleedsheikh30@users.noreply.github.com>
2024-09-10 03:35:10 +0500

    Update server.js

--- erp-inventory/server/server.js
+++ erp-inventory/server/server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
 // Middleware
 app.use(cors({
   origin: '*', // Allow requests from any origin
