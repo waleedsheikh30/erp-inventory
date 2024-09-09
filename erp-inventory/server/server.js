ce9be3bcb4be628f4c4b8e01d90d2ae42cb490c0
waleedsheikh000 <sheikhbrothers1090@gmail.com>
2024-09-10 02:51:48 +0500

    updated

--- erp-inventory/server/server.js
+++ erp-inventory/server/server.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect to MongoDB
