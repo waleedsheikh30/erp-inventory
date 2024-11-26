67d4d9a0ae13fa0d5e4efa431caf4d53c66b24e6
waleedsheikh000 <sheikhbrothers1090@gmail.com>
2024-11-26 06:38:58 +0500

    msg

--- erp-inventory/server/server.js
+++ erp-inventory/server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://erp-inventory.onrender.com'],
   credentials: true,
 }));
 app.use(express.json());
